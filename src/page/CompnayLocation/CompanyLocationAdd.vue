c3ece4675117915c32ecf040c10e7b60fa713ed8
omarbr44 <omarbr44@users.noreply.github.com>
2024-11-01 22:13:08 +0300

    create location pages

---
+++ src/page/CompnayLocation/CompanyLocationAdd.vue
@@ -0,0 +1,210 @@
+<template>
+    <div>
+        <NavBarComponent />
+        <div class="flex justify-stretch">
+            <div class="w-[80%] rtl-d px-10 bg-[#f9f9f9] pt-28">
+                <h1 class="text-xl font-bold text-site-blue mb-5">إضافة موقع جديد</h1>
+                <div class="w-full p-4 border border-[#DADADA] rounded-2xl flex flex-wrap gap-y-4">
+                    <div class="w-[45%]">
+                        <p class="text-black font-semibold w-[50%] mb-3">اسم الموقع <span class="text-red-500">*</span></p>
+                        <input 
+                        name="location-name" 
+                        type="text" 
+                        class="w-full relative bg-[#f9f9f9] border border-[#BDBDBD] p-2 rounded-xl"
+                        placeholder=" مثال : باقة ٩ العزيزية">
+                    </div>
+                    <div class="w-[8%]"></div>
+                    <div class="w-[45%]">
+                        <p class="text-black font-semibold w-[50%] mb-3">السعة<span class="text-red-500">*</span></p>
+                        <input 
+                        name="location-size" 
+                        type="number" 
+                        class="w-full relative bg-[#f9f9f9] border border-[#BDBDBD] p-2 rounded-xl"
+                        placeholder="مثال : 1032 ">
+                    </div>
+                    <div class="w-[45%]">
+                        <p class="text-black font-semibold w-[50%] mb-3">رقم المخيم<span class="text-red-500">*</span></p>
+                        <input 
+                        name="number" 
+                        type="number" 
+                        class="w-full relative bg-[#f9f9f9] border border-[#BDBDBD] p-2 rounded-xl"
+                        placeholder="مثال : 7121 ">
+                    </div>
+                    <div class="w-[8%]"></div>
+                    <div class="w-[45%]">
+                        <p class="text-black font-semibold w-[50%] mb-3">رقم الواتس<span class="text-red-500">*</span></p>
+                        <input 
+                        name="whatsnumber" 
+                        type="number" 
+                        class="w-full relative bg-[#f9f9f9] border border-[#BDBDBD] p-2 rounded-xl"
+                        placeholder="مثال : 56877869">
+                    </div>
+                    <div class="w-[45%]">
+                        <p class="text-black font-semibold w-[50%] mb-3">عدد الرجال</p>
+                        <input 
+                        name="man-number" 
+                        type="number" 
+                        class="w-full relative bg-[#f9f9f9] border border-[#BDBDBD] p-2 rounded-xl"
+                        placeholder="مثال : 99">
+                    </div>
+                    <div class="w-[8%]"></div>
+                    <div class="w-[45%]">
+                        <p class="text-black font-semibold w-[50%] mb-3">عدد النساء</p>
+                        <input 
+                        name="woman-number" 
+                        type="number" 
+                        class="w-full relative bg-[#f9f9f9] border border-[#BDBDBD] p-2 rounded-xl"
+                        placeholder="مثال : 99">
+                    </div>
+                    <div class="w-[45%]">
+                        <p class="text-black font-semibold w-[50%] mb-3">تحديد الإدارة</p>
+                        <input 
+                        name="managment-number" 
+                        type="number" 
+                        class="w-full relative bg-[#f9f9f9] border border-[#BDBDBD] p-2 rounded-xl"
+                        placeholder="مثال : 99">
+                    </div>
+                    <div class="w-[8%]"></div>
+                    <div class="w-[45%]">
+                        <p class="text-black font-semibold w-[50%] mb-3">تحديد الخدمات</p>
+                        <input 
+                        name="service-number" 
+                        type="number" 
+                        class="w-full relative bg-[#f9f9f9] border border-[#BDBDBD] p-2 rounded-xl"
+                        placeholder="مثال : 88">
+                    </div>
+                    <div class="">
+                        <p class="text-black font-semibold mb-3">هل يتوفر مع  هذا الموقع خدمة القطار ؟ </p>
+                        <div class="flex items-center p-2">
+                            <input
+                            name="train-option" 
+                            type="radio"
+                            value="yes"
+                            class="relative bg-[#f9f9f9] border border-[#BDBDBD] p-2 rounded-xl size-4">
+                            <label class="ml-5 mr-2" >نعم</label>
+                            <input 
+                            name="train-option" 
+                            type="radio"
+                            value="no"
+                            class="relative bg-[#f9f9f9] border border-[#BDBDBD] p-2 rounded-xl size-4">
+                            <label class="mr-2">لا</label>
+
+                        </div>
+                    </div>
+                </div>
+                <h1 class="text-xl font-bold text-site-blue my-5">موقع منى</h1>
+                <div class="w-full p-4 border border-[#DADADA] rounded-2xl flex flex-wrap gap-y-4">
+                    <div class="w-[45%]">
+                        <div class="relative" >
+                            <p class="text-black font-semibold w-[50%] mb-3 mt-5">رابط موقع منى</p>
+                            <input 
+                            name="link" 
+                            type="text" 
+                            class="w-full bg-[#f9f9f9] border border-[#BDBDBD] p-2 rounded-xl"
+                            placeholder="اضف رابط الموقع من خرائط قوقل">
+                            <LocationIcon />
+                        </div>
+                    </div>
+                    <div class="w-[8%]"></div>
+                    <div class="w-[45%]">
+                        <p class="text-black font-semibold w-[50%] mb-3">عنوان منى</p>
+                        <textarea
+                         name=""
+                         rows="6" 
+                         class="w-full relative bg-[#f9f9f9] border border-[#BDBDBD] p-2 rounded-xl"
+                         placeholder="مثال : مخيم رقم D19 (6-5) فئة (هـ) مقابل محطة مزدلفة 3
+Camp No. ( D19 ) (6-5) (E)Opposite Muzdalifah Train Station No.3"
+                        ></textarea>
+                    </div>
+                </div>
+                <h1 class="text-xl font-bold text-site-blue my-5">موقع عرفة</h1>
+                <div class="w-full p-4 border border-[#DADADA] rounded-2xl flex flex-wrap gap-y-4">
+                    <div class="w-[45%]">
+                        <div class="relative" >
+                            <p class="text-black font-semibold w-[50%] mb-3 mt-5">رابط موقع عرفة</p>
+                            <input 
+                            name="link" 
+                            type="text" 
+                            class="w-full bg-[#f9f9f9] border border-[#BDBDBD] p-2 rounded-xl"
+                            placeholder="اضف رابط الموقع من خرائط قوقل">
+                            <LocationIcon />
+                        </div>
+                    </div>
+                    <div class="w-[8%]"></div>
+                    <div class="w-[45%]">
+                        <p class="text-black font-semibold w-[50%] mb-3">عنوان عرفة</p>
+                        <textarea
+                         name=""
+                         rows="6" 
+                         class="w-full relative bg-[#f9f9f9] border border-[#BDBDBD] p-2 rounded-xl"
+                         placeholder="مثال : مخيم Camp No. ( D19 ) (6-5) (E)Opposite Muzdalifah Train Station No.3"
+                        ></textarea>
+                    </div>
+                </div>
+                <h1 class="text-xl font-bold text-site-blue my-5">موقع مزدلفة</h1>
+                <div class="w-full p-4 border border-[#DADADA] rounded-2xl flex flex-wrap gap-y-4">
+                    <div class="w-[45%]">
+                        <div class="relative" >
+                            <p class="text-black font-semibold w-[50%] mb-3 mt-5">رابط موقع مزدلفة</p>
+                            <input 
+                            name="link" 
+                            type="text" 
+                            class="w-full bg-[#f9f9f9] border border-[#BDBDBD] p-2 rounded-xl"
+                            placeholder="اضف رابط الموقع من خرائط قوقل">
+                            <LocationIcon />
+                        </div>
+                    </div>
+                    <div class="w-[8%]"></div>
+                    <div class="w-[45%]">
+                        <p class="text-black font-semibold w-[50%] mb-3">عنوان مزدلفة</p>
+                        <textarea
+                         name=""
+                         rows="6" 
+                         class="w-full relative bg-[#f9f9f9] border border-[#BDBDBD] p-2 rounded-xl"
+                         placeholder="مثال : مخيم Camp No. ( D19 ) (6-5) (E)Opposite Muzdalifah Train Station No.3"
+                        ></textarea>
+                    </div>
+                </div>
+                <div class="flex items-center justify-between my-10 text-lg font-bold">
+                    <button class=" bg-site-blue text-white py-3 px-10 rounded-lg">إضافة</button>
+                    <RouterLink to="/company-locations" class="text-red-700 py-4 px-8 ">
+                        إلغاء
+                    </RouterLink>
+                </div>
+            </div>
+            <SideBarComponent />
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import NavBarComponent from '../../components/Company/NavBarComponent.vue';
+import SideBarComponent from '../../components/SideBarComponent.vue';
+import ArrowUpIcon from '../../components/icon/ArrowUpIcon.vue';
+import ArrowDownIcon from '../../components/icon/ArrowDownIcon.vue';
+import DownloadIcon from '../../components/icon/DownloadIcon.vue';
+import FileInputComponent from '../../components/Base/FileInputComponent.vue';
+import LocationIcon from '../../components/icon/LocationIcon.vue';
+
+const hideSection = ref(null)
+const sectioIsHide = ref(true)
+const toggleSection = () => {
+    if(hideSection.value.classList.contains('h-0')) {
+        hideSection.value.classList.add('h-auto')
+        hideSection.value.classList.remove('h-0')
+        sectioIsHide.value = false
+    }
+    else {
+        hideSection.value.classList.add('h-0')
+        hideSection.value.classList.remove('h-auto')
+        sectioIsHide.value = true
+    }
+}
+</script>
+
+<style>
+.dp__input {
+    background: #f9f9f9 !important;
+}
+</style>
---
+++ src/page/CompnayLocation/CompanyLocationView.vue
@@ -0,0 +1,149 @@
+<template>
+    <div>
+        <NavBarComponent />
+        <div class="flex justify-stretch">
+            <div class="w-[80%] rtl-d px-5 bg-[#f9f9f9] pt-28">
+                <h1 class="text-site-blue text-xl font-semibold">مواقع المخيمات التابعة للشركة</h1>
+                <RouterLink to="company-location-add" class="bg-site-blue w-fit text-white rounded-xl p-4 flex items-center gap-2 my-8">
+                    <PlusIcon />
+                    <span class="font-bold">إضافة موقع جديد</span>
+                </RouterLink>
+                <TableComponent
+                :modalVisible="modalVisible"
+                @closeModal="modalVisible = false"
+                :canDelete="false">
+                    <template v-slot:header>
+                        <tr>
+                            <th scope="col">اسم الموقع</th>
+                            <th scope="col">السعة</th>
+                            <th scope="col">إدارة الباصات</th>
+                            <th scope="col">الخيام</th>
+                            <th scope="col">عدد الرجال</th>
+                            <th scope="col">عدد النساء</th>
+                            <th scope="col">القطار</th>
+                            <th scope="col">العمليات</th>
+                        </tr>
+                    </template>
+                    <template v-slot:body>
+                        <tr>
+                <th scope="row">باقة (4) سكن العزيزية</th>
+                <td>1124</td>
+                <td>24</td>
+                <td>72</td>
+                <td>729</td>
+                <td>580</td>
+                <td class="font-bold text-green-700 bg-green-300">متوفر</td>
+                <td class="flex items-center justify-center gap-5">
+                    <button class="flex items-center gap-2">
+                        <EditIcon />
+                        <span class="text-[#46814F]">تعديل</span>
+                    </button>
+                    <button @click="modalVisible = true" class="flex items-center gap-2">
+                        <DeleteIcon />
+                        <span class="text-[#FF3F3F]">حذف</span>
+                    </button>
+                </td>
+                </tr>
+                        <tr>
+                <th scope="row">باقة (4) سكن العزيزية</th>
+                <td>1124</td>
+                <td>24</td>
+                <td>72</td>
+                <td>729</td>
+                <td>580</td>
+                <td class="font-bold text-green-700 bg-green-300">متوفر</td>
+                <td class="flex items-center justify-center gap-5">
+                    <button class="flex items-center gap-2">
+                        <EditIcon />
+                        <span class="text-[#46814F]">تعديل</span>
+                    </button>
+                    <button @click="modalVisible = true" class="flex items-center gap-2">
+                        <DeleteIcon />
+                        <span class="text-[#FF3F3F]">حذف</span>
+                    </button>
+                </td>
+                </tr>
+                        <tr>
+                <th scope="row">باقة (4) سكن العزيزية</th>
+                <td>1124</td>
+                <td>24</td>
+                <td>72</td>
+                <td>729</td>
+                <td>580</td>
+                <td class="font-bold text-green-700 bg-green-300">متوفر</td>
+                <td class="flex items-center justify-center gap-5">
+                    <button class="flex items-center gap-2">
+                        <EditIcon />
+                        <span class="text-[#46814F]">تعديل</span>
+                    </button>
+                    <button @click="modalVisible = true" class="flex items-center gap-2">
+                        <DeleteIcon />
+                        <span class="text-[#FF3F3F]">حذف</span>
+                    </button>
+                </td>
+                </tr>
+                        <tr>
+                <th scope="row">باقة (4) سكن العزيزية</th>
+                <td>1124</td>
+                <td>24</td>
+                <td>72</td>
+                <td>729</td>
+                <td>580</td>
+                <td class="font-bold text-green-700 bg-green-300">متوفر</td>
+                <td class="flex items-center justify-center gap-5">
+                    <button class="flex items-center gap-2">
+                        <EditIcon />
+                        <span class="text-[#46814F]">تعديل</span>
+                    </button>
+                    <button @click="modalVisible = true" class="flex items-center gap-2">
+                        <DeleteIcon />
+                        <span class="text-[#FF3F3F]">حذف</span>
+                    </button>
+                </td>
+                </tr>
+
+                
+                    </template>
+                    <template v-slot:delete-modal-1>
+                        <h1 class="text-center text-3xl font-bold mb-5">هل أنت متأكد أنك ترغب في حذف الإدارة المحددة؟</h1>
+                        <p class="text-center mb-8">سيتم حذف الإدارة بأكملها، وستتم إزالة جميع البيانات المتعلقة بهذه الإدارة. و لا يمكن استعادة هذه المعلومات بعد الحذف.</p>
+                    </template>
+                    <template v-slot:delete-modal-2>
+                        <h1 class="text-xl font-bold mb-5">نعتذر، لا يمكن حذف هذه الإدارة في الوقت الحالي. يبدو أنها مرتبطة بموظفين أو تم تعيين مدير عليها. يرجى اتخاذ الخطوات التالية قبل محاولة الحذف:
+                        </h1>
+                        <p class="mb-8">
+                            1. تحقق من فصل جميع الموظفين المرتبطين بالإدارة.
+                            <br>
+                            2. تأكد من عدم تعيين مدير على هذه الإدارة.
+                            <br>
+                            3. بعد الانتهاء من الخطوات السابقة، يمكنك محاولة حذف الإدارة.
+                        </p>
+                        <p>يرجى ملاحظة أن عملية الحذف ستؤدي إلى إزالة جميع البيانات ذات الصلة ولا يمكن استعادتها بعد الحذف.</p>                              
+                    </template>
+                </TableComponent>
+            </div>
+            <SideBarComponent />
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { RouterLink } from 'vue-router';
+import TableComponent from '../../components/Base/tableComponent.vue';
+import NavBarComponent from '../../components/Company/NavBarComponent.vue';
+import PlusIcon from '../../components/icon/PlusIcon.vue';
+import EditIcon from '../../components/icon/EditIcon.vue'
+import DeleteIcon from '../../components/icon/DeleteIcon.vue'
+import SideBarComponent from '../../components/SideBarComponent.vue';
+
+const modalVisible = ref(false)
+
+const print = ()=>{
+    window.print()
+}
+</script>
+
+<style scoped>
+
+</style>

3ca90602ac92bba7610c1aa9537e6d13f4291edd
omarbr44 <omarbr44@users.noreply.github.com>
2024-11-05 10:18:10 +0300

    ..

--- src/page/Hajj/HajjInfo/HajjInfoView.vue
+++ src/page/Hajj/HajjInfo/HajjInfoView.vue
@@ -3,29 +3,49 @@
         <div v-if="loadPage">
             <div class="rtl-d px-5 pt-28">
                 <h1 class="text-site-blue text-xl font-semibold">معلومات جميع الحجاج</h1>
-                <RouterLink to="hajj-info-add/add" class="bg-site-blue w-fit text-white rounded-xl p-4 flex items-center gap-2 my-8">
+                <button class="bg-site-blue w-fit text-white rounded-xl p-4 flex items-center gap-2 my-8">
                     <DownloadIcon color="white" />
                     <span class="font-bold">إستيراد  الحجاج</span>
-                </RouterLink>
+                </button>
                 <p class=" text-site-text-grey font-medium my-5">*قم برفع ملف اكسيل لإستيراد معلومات حجاجك</p>
                 <div class="flex items-center gap-5 mb-10">
                     <button id="menu-activator" class="flex flex-col items-center gap-1">
                         <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M18 0V2H17L11 11V18H7V11L1 2H0V0H18Z" fill="#5FC1CF"/>
                         </svg>
-                        <p class="text-site-blue">القطار</p>
+                        <p class="text-site-blue">صنف ب</p>
                     </button>
                     <v-menu activator="#menu-activator" :close-on-content-click="false">
                         <div class="bg-white shadow-md rounded-md">
-                            <p class="text-site-blue font-bold rtl-d p-3">حالة القطار</p>
+                            <p class="text-site-blue font-bold rtl-d p-3">الموقع</p>
                             <div class="flex gap-10 items-center px-10 pt-1 mb-1 justify-end flex-wrap gap-y-5 w-[23rem]">
                                 <div class="flex items-center">
-                                    <label for="default-radio-1" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">الكل</label>
+                                    <label for="default-radio-1" class="mx-1 text-sm font-medium text-gray-900 dark:text-gray-300">الكل</label>
                                     <input id="default-radio-1" type="radio" value="" v-model="trainStatusFilter" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
                                 </div>
-                                <div  v-for="(status,index) in trainStatuses" :key="index" class="flex items-center gap-1">
-                                    <label for="default-radio-1" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">{{status.name}}</label>
-                                    <input id="default-radio-1" type="radio" :value="status.id" v-model="trainStatusFilter" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                <div class="flex items-center">
+                                    <label for="default-radio-1" class="mx-1 text-sm font-medium text-gray-900 dark:text-gray-300">باقة (4) الخيام المطورة</label>
+                                    <input id="default-radio-1" type="radio" value="" v-model="trainStatusFilter" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                </div>
+                                <div class="flex items-center">
+                                    <label for="default-radio-1" class="mx-1 text-sm font-medium text-gray-900 dark:text-gray-300">باقة (٣) الخيام العزيزية</label>
+                                    <input id="default-radio-1" type="radio" value="" v-model="trainStatusFilter" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                </div>
+                            </div>
+                            <hr>
+                            <p class="text-site-blue font-bold rtl-d p-3">اسم الفرع</p>
+                            <div class="flex gap-10 items-center px-10 pt-1 mb-1 justify-end flex-wrap gap-y-5 w-[23rem]">
+                                <div class="flex items-center">
+                                    <label for="default-radio-1" class="mx-1 text-sm font-medium text-gray-900 dark:text-gray-300">جدة</label>
+                                    <input id="default-radio-1" type="radio" value="" v-model="trainStatusFilter" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                </div>
+                                <div class="flex items-center">
+                                    <label for="default-radio-1" class="mx-1 text-sm font-medium text-gray-900 dark:text-gray-300">الرياض</label>
+                                    <input id="default-radio-1" type="radio" value="" v-model="trainStatusFilter" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                </div>
+                                <div class="flex items-center">
+                                    <label for="default-radio-1" class="mx-1 text-sm font-medium text-gray-900 dark:text-gray-300">المدينة المنورة</label>
+                                    <input id="default-radio-1" type="radio" value="" v-model="trainStatusFilter" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
                                 </div>
                             </div>
                         </div>
@@ -87,7 +107,7 @@
                                 </button>
                             </td>
                         </tr> -->
-                        <tr>
+                        <tr v-for="index in 5" >
                             <td>28858326</td>
                             <td>ABDUL HAMEED MUHAMMAD SIDDIQ</td>
                             <td>34</td>

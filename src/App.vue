94509631247e79115ebf0ba30dab28db00d6ced5
omarbr44 <omarbr44@users.noreply.github.com>
2024-12-07 21:58:21 +0300

    edit notifications

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
   import NavBarComponent4 from './components/Bed/NavBarComponent.vue';
   import SideBarComponent from './components/SideBarComponent.vue';
   import { computed, ref } from 'vue';
+import Xicon from './components/icon/Xicon.vue';
 
 
   const route = useRoute()
@@ -36,7 +37,24 @@
 </script>
 
 <template>
-    <notifications  />
+    <notifications>
+      <template #body="props">
+        <div class="vue-notification"
+             :class="props.item.type == 'error' ? 'notification-error' : 'notification-success'">
+          <Xicon v-if="props.item.type == 'error'" :notification="true" />
+          <img v-else src="/img/checked.png" alt="">
+          <p class="title font-bold text-xl mt-2">
+            {{ props.item.title }}
+          </p>
+<!--           <button class="close" @click="props.close">
+            <i class="fa fa-fw fa-close"></i>
+          </button> -->
+          <p class="title font-bold">
+            {{ props.item.text }}
+          </p>
+        </div>
+      </template>
+    </notifications>
   <div class="overflow-x-hidden">
     <component 
       :is="showActiveNav"
@@ -67,7 +85,27 @@
             background-clip: content-box;
             background-color: #48b4c3;
         }
-.vue-notification{
-  font-size: 1.2rem !important;
+
+        
+        .vue-notification {
+  margin: 0 5px 5px;
+  padding: 10px;
+  font-size: 12px;
+  color: #ffffff;
 }
+
+  .notification-success {
+    background: #68cd86 !important;
+    border-left-color: #42a85f !important;
+  }
+
+  .notification-warn {
+    background: #ffb648;
+    border-left-color: #f48a06;
+  }
+
+  .notification-error {
+    background: #e54d42 !important;
+    border-left-color: #b82e24 !important;
+  }
 </style>
